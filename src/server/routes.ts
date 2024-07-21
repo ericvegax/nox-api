12ef09875d436e14d70ce7d02301731c1afb72b1
ericvegax <ericvegax@users.noreply.github.com>
2024-07-21 02:54:43 -0400

    updated routes.ts

--- src/server/routes.ts
+++ src/server/routes.ts
@@ -1,7 +1,7 @@
 import ChatFilter from "../route/chat/chat-filter-route"
-import LoginRoute from "../route/login/login-route"
+import Login from "../route/login/login-route"
 
 export {
     ChatFilter,
-    LoginRoute
+    Login
 }
